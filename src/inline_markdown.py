8addfb65d4534f89a33c69fc985f156a2688ce2e
Sean Moakes <mail@seanmoakes.com>
2025-01-17 15:39:25 +0000

    text to textnodes added

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -88,3 +88,14 @@ def extract_markdown_links(text):
     pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
     return matches
+
+
+def text_to_textnodes(text):
+    nodes = []
+    nodes.append(TextNode(text, TextType.TEXT))
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    return nodes
