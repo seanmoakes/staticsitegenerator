0814aebddbb36ed9fd26d5ea91d18701cd9f2042
Sean Moakes <mail@seanmoakes.com>
2025-08-11 14:06:26 +0100

    fix italics

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -97,5 +97,5 @@ def text_to_textnodes(text):
     nodes = split_nodes_link(nodes)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     return nodes
